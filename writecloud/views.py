2cdeff7f42f11051df16157822e93de1ebfa4eef
Matilda Bradley <2470620b@student.gla.ac.uk>
2021-03-16 00:15:41 +0000

    matilda 2

--- writecloud/views.py
+++ writecloud/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import redirect 
 
-# Create your views here.
+def writecloud_user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        writecloud_user = authenticate(username=username, password=password)
+
+        if writecloud_user:
+
+            if writecloud_user.is_active:
+                login(request, user)
+                return redirect(reverse('
+
+            else:
+                return HttpResponse("Your WriteCloud account has been disabled.")
+
+            
+
+        
