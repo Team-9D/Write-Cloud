a4e24d538fffec3a4391c067f597f9d4d8d5a235
kiril-bikov <2444048B@student.gla.ac.uk>
2021-04-05 10:53:52 +0300

    Tests continued

--- writecloud/tests.py
+++ writecloud/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 from writecloud.models import Story, Page, Review, User
 
@@ -96,7 +97,7 @@ class ReviewModelTest(TestCase):
         Story.objects.create(title='First_Test_Story', subtitle='This is the first test story', length='2',
                              author=User.objects.last())
         User.objects.create_user(username='Test_name1', email='', password='0000')
-        Review.objects.create(stars ='4', body='Great one', author=User.objects.last(), story=Story.objects.last())
+        Review.objects.create(stars='4', body='Great one', author=User.objects.last(), story=Story.objects.last())
 
     def test_stars(self):
         review = Review.objects.last()
@@ -117,3 +118,73 @@ class ReviewModelTest(TestCase):
         review = Review.objects.last()
         field_label = review._meta.get_field('author').verbose_name
         self.assertEqual(field_label, 'author')
+
+
+class LoginViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create 5 users for tests
+        number_of_users = 5
+
+        for user_id in range(number_of_users):
+            User.objects.create_user(
+                username=f'TestName{user_id}',
+                password=f'TestPass{user_id}',
+                email=''
+            )
+
+    # Test login location
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    # Test successful login of a created user
+    def test_view_url_accessible_by_name(self):
+        username = User.objects.last().username
+        password = User.objects.last().password
+        response = self.client.post('/', username, password)
+        self.client.login(username=username, password=password)
+        self.user = User.objects.get(username=username)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.last(), self.user)
+
+    # Test if correct template used
+    def test_view_uses_correct_template(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'writecloud/login.html')
+
+
+class IndexViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create 3 users for tests
+        number_of_users = 3
+
+        for user_id in range(number_of_users):
+            User.objects.create_user(
+                username=f'TestName{user_id}',
+                password=f'TestPass{user_id}',
+                email=''
+            )
+
+    # Test if redirected when user is not logged in
+    def test_redirect_no_user(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 302)
+
+    # Test correct access when user is logged in
+    def test_view_url_exists_logged_in(self):
+        username = User.objects.last().username
+        password = User.objects.last().password
+        self.client.force_login(User.objects.last(), backend=None)
+        self.client.post('/', username, password)
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+    # Test if correct template used
+    def test_view_uses_correct_template(self):
+        self.client.force_login(User.objects.last(), backend=None)
+        self.client.post('/', User.objects.last().username, User.objects.last().password)
+        response = self.client.get('/home/')
+        self.assertTemplateUsed(response, 'writecloud/index.html')
